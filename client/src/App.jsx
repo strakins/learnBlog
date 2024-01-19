c711757d3c7fcf0709e4442042ba2ddd887ea2e9
strakins <akinolablessing25@gmail.com>
2024-01-19 11:54:18 +0100

    Initail setup of blog app with react-vite and tailwinf css + landing page template created

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 text-center text-4xl">Hello We are creating a Blog</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
