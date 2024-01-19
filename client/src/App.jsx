d29c1a3d8034895e53bd20109d7c550548f5abc3
strakins <akinolablessing25@gmail.com>
2024-01-19 12:43:35 +0100

    Created Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Projects from './pages/Projects';
+
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1 className="text-red-500 text-center text-4xl">Hello We are creating a Blog</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={< Login />} />
+        <Route path='/register' element={< Register />} />
+        <Route path='/projects' element={< Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
