5f128fb69175c2346abb0497091a4bddaa252cca
strakins <akinolablessing25@gmail.com>
2024-01-19 12:53:42 +0100

    Created Header Template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
