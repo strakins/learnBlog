83534c62f3d0f3d53edf0f80a2cb4f118d747ba4
strakins <akinolablessing25@gmail.com>
2024-01-21 21:22:13 +0100

    Error Middleware created

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,23 +1,32 @@
+import { errorHandler } from '../middleware/error.js';
 import User from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
 
 export const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
-    if(!username || !email || !password || username === ''|| email === '' || password === '') {
-        return res.status(400).json({message: "Please fill all fields"})
+    if ( 
+        !username || 
+        !email || 
+        !password || 
+        username === ''|| 
+        email === '' || 
+        password === ''
+    ) {
+        next(errorHandler(400, 'All fields are required'));
     }
 
+    const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({
         username, 
         email, 
-        password
+        password: hashedPassword
     });
 
     try {
         await newUser.save();
-        res.json('Signup Successful')
+        res.json(`Signup Successful for ${email}`)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        next(error);
     }
 
 } 
