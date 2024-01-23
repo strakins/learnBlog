d25b2189fe1e45219ef5050183dc9810ea7ccc52
strakins <akinolablessing25@gmail.com>
2024-01-23 22:07:06 +0100

    Footer Created with responsive screens

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import {Footer} from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+
+const FooterCom = () => {
+  return (
+    <Footer container className='border border-t-8 border-purple-500'>
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="my-5 ">
+            <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl 
+              font-semibold dark:text-white'>
+                <span className='px-2 py-1 bg-gradient-to-tr from-blue-600 via-green-500 to-purple-500 rounded-md text-white'>STRAKINS</span>
+                BLOG
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  My Projects
+                </Footer.Link>
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  About Us
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Our Offers' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  My Projects
+                </Footer.Link>
+              
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  About Us
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Contact Us' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  My Projects
+                </Footer.Link>
+              
+                <Footer.Link href='' target='_blank' rel='noopener noreffere'>
+                  About Us
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright 
+            href='https://strakins-portfolio.vercel.app/' 
+            by="Strakin's Blog" 
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-4 mt-2 md:mt-4 sm:justify-center">
+            <Footer.Icon href='' icon={FaTwitter} />
+            <Footer.Icon href='' icon={FaLinkedin} />
+            <Footer.Icon href='' icon={FaGithub} />
+          </div>
+        </div>
+
+      </div>
+    </Footer>
+  )
+}
+
+export default FooterCom
